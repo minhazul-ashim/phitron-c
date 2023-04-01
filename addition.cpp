5bb949d7ee75fbaa163eec6f4c2041915daa0e63
Md. Minhazul Haque <ashim.ashim7470@gmail.com>
2023-04-01 21:39:53 +0600

    checking the git ignore

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a;
+    int b;
+    int c;
+    cout << "Enter the value of a";
+    cin >> a;
+    cout << "Enter the value of b";
+    cin >> b;
+    c = a + b;
+    cout << 'Addition of A and B is' << c;
+    return 0;
+}
